98a439e1258b2b70ce768ce8eee730b8884ff6ef
KasimovAslbek <KasimovAslbek1994@mail.ru>
2023-08-10 16:29:57 +0500

    Hello is added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Footer from "./components/Footer";
-
-
+import Meeting from "./components/Meeting";
 
 function App() {
+  const [showMeeting, setShowMeeting] = useState(true);
+
+  useEffect(() => {
+    // After 6 seconds, hide the meeting component
+    const timer = setTimeout(() => {
+      setShowMeeting(false);
+    }, 5000);
+
+    // Clean up the timer when the component unmounts or the effect runs again
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App  overflow-hidden relative bg-[#fafafa]">
-        <Navbar/>
-        <Home/>
-        <Skills/>
-        <Projects/>
-        <About/>
-        <Footer/>
+      {showMeeting && <Meeting />}
+      {!showMeeting && (
+        <>
+          <Navbar />
+          <Home />
+          <Skills />
+          <Projects />
+          <About />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
+
